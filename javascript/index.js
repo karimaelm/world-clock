2754ea0c908fbfc4ec3d4f2d0ef6e8e9661b7a43
Karima <karimaelm.git@gmail.com>
2024-02-05 21:05:42 +0100

    added final css

--- javascript/index.js
+++ javascript/index.js
@@ -45,7 +45,7 @@ function updateCity(event) {
     <div class="time">${cityTime.format("h:mm:ss")}
      <small>${cityTime.format("A")}</small> </div>
     </div>
-    <a href="/">All cities</a>
+    <a href="/">   All cities</a>
     `;
 }
 
